7be906da95a25933d9e4166b123b39fccea3b5ee
Bert Belder <bertbelder@gmail.com>
2017-06-06 16:40:03 +0200

    md5: integrate istgt_md5.* with md5.*

--- src/istgt_md5.h
+++ src/istgt_md5.h
@@ -1,20 +1,27 @@
 /*
- * Copyright (C) 2008-2014 Daisuke Aoyama <aoyama@peach.ne.jp>.
+ * Copyright (c) 1995 - 2001 Kungliga Tekniska Högskolan
+ * (Royal Institute of Technology, Stockholm, Sweden).
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -22,22 +29,26 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
  */
+
+/* $Heimdal: md5.h,v 1.8 2001/01/29 02:08:57 assar Exp $
+   $NetBSD: md5.h,v 1.1.1.4 2002/09/12 12:41:42 joda Exp $ */
 
 #ifndef ISTGT_MD5_H
 #define ISTGT_MD5_H
 
-#include <stddef.h>
-
-#include "md5.h"
+#include <stdint.h>
 
 #define ISTGT_MD5DIGEST_LEN 16
 
-typedef struct istgt_md5ctx_t { struct md5 state; } ISTGT_MD5CTX;
+typedef struct istgt_md5ctx_t {
+  unsigned int sz[2];
+  uint32_t counter[4];
+  unsigned char save[64];
+} ISTGT_MD5CTX;
 
-int istgt_md5init(ISTGT_MD5CTX* md5ctx);
-int istgt_md5final(void* md5, ISTGT_MD5CTX* md5ctx);
-int istgt_md5update(ISTGT_MD5CTX* md5ctx, const void* data, size_t len);
+void istgt_md5init(ISTGT_MD5CTX* md5ctx);
+void istgt_md5update(ISTGT_MD5CTX* md5ctx, const void* data, size_t len);
+void istgt_md5final(void* md5, ISTGT_MD5CTX* md5ctx);
 
 #endif /* ISTGT_MD5_H */
